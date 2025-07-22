2de5e09244ae6a9e1c29d65ce7f4f9eb9ce956de
Rocko <roc26002w@gmail.com>
2025-07-22 14:23:52 +0800

    feat: 新增 git 檔案搜尋 shell 並整合 tig 快捷鍵
      * 新增 git/shell/find-file.sh，支援以檔名搜尋並預覽 commit
      * tig/.tigrc 增加 <Ctrl-l> 快捷鍵，呼叫搜尋腳本

---
+++ git/shell/find-file.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+clear
+function find_file_view() {
+       FILE_NAME=$1
+       git log "*/${FILE_NAME}" 2>/dev/null | cut -d" " -f 2  | grep -E '^[0-9a-f]{40}$' | \
+       fzf --ansi --no-sort --reverse --tiebreak=index --preview \
+       'f() { set -- $(echo -- "$@" | grep -o "[a-f0-9]\{7\}"); [ $# -eq 0 ] || git log --stat --color=always -n 1 $1 ; }; f {}' \
+       --bind "ctrl-y:execute:echo {} | grep -o '[a-f0-9]\{7\}' | head -1 | tr -d '\n' | pbcopy"\
+       --bind "j:preview-down,k:preview-up,J:preview-page-down,K:preview-page-up,ctrl-j:down,ctrl-k:up,q:abort"\
+       --bind "ctrl-o:execute:
+                    (grep -o '[a-f0-9]\{7\}' | head -1 |
+                    xargs -I % sh -c 'git show --color=always % | less -R') << 'FZF-EOF'
+                    {}
+        FZF-EOF" \
+        --preview-window=right:80%
+}
+
+find_file_view "$@"
